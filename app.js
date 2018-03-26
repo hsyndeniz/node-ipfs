b96925b7dc64e79e086a4997e3d61255b5cb84d3
Hüseyin TERKİR <hsynterkr@gmail.com>
2018-03-26 04:50:07 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var validator = require('express-validator');
+
+var indexRouter = require('./routes/index');
+var uploadRouter = require('./routes/upload');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(validator());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/upload', uploadRouter);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/upload.js
@@ -0,0 +1,73 @@
+const ipfsAPI = require('ipfs-api');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+var express = require('express');
+var router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, path.join(__dirname, 'uploads'));
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}.${file.mimetype.split('/')[1]}`);
+  },
+});
+
+const upload = multer({ storage });
+
+const ipfs = ipfsAPI({
+  host: 'localhost',
+  port: 5001,
+  protocol: 'http',
+});
+
+/*  upload POST endpoint */
+router.post('/', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(422).json({
+      error: 'File needs to be provided.',
+    });
+  }
+
+  const mime = req.file.mimetype;
+  if (mime.split('/')[0] !== 'image') {
+    fs.unlink(req.file.path);
+
+    return res.status(422).json({
+      error: 'File needs to be an image.',
+    });
+  }
+
+  const maxSize = req.app.fileSize;
+  const fileSize = req.file.size;
+  if (fileSize > maxSize) {
+    fs.unlink(req.file.path);
+
+    return res.status(422).json({
+      error: `Image needs to be smaller than ${maxSize} bytes.`,
+    });
+  }
+
+  const data = fs.readFileSync(req.file.path);
+  return ipfs.add(data, (err, files) => {
+    fs.unlink(req.file.path);
+    if (files) {
+      return res.json({
+        hash: files[0].hash,
+      });
+    }
+
+    return res.status(500).json({
+      error: err,
+    });
+  });
+});
+
+/*  upload GET endpoint. */
+router.get('/', function(req, res, next) {
+  res.send('Upload endpoint!');
+});
+
+module.exports = router;
